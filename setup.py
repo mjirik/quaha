157bd882da091446618dc2030072bc49ced5f972
Miroslav Jiřík <mjirik@kky.zcu.cz>
2015-11-05 14:41:26 +0100

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 # Always prefer setuptools over distutils
 from os import path
-__VERSION__ = '0.0.4'
+__VERSION__ = '0.0.5'
 
 here = path.abspath(path.dirname(__file__))
 setup(
